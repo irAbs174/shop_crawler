29b92b18539c6a016c4db33dd9d0f5bc20e53979
irAbs174 <damerchiloa@gmail.com>
2024-06-08 04:41:46 +0000

    remove django-chanells

--- app/backend/core/locals.py
+++ app/backend/core/locals.py
@@ -1,8 +1,8 @@
 # SECURITY WARNING: ALLOWED_HOSTS !
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '8000-cs-415202498594-default.cs-europe-west1-xedi.cloudshell.dev/']
 
 # SECURITY WARNING: CSRF_TRUSTED_ORIGINS !
-CSRF_TRUSTED_ORIGINS = []
+CSRF_TRUSTED_ORIGINS = ['http://8000-cs-415202498594-default.cs-europe-west1-xedi.cloudshell.dev',]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
--- docs/install.md
+++
@@ -1,2 +0,0 @@
-python -m pip install -U 'channels[daphne]'
-pip3 install django flask bs4 requests channels_redis
