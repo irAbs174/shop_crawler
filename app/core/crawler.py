16bff84c8d705e035eb8f7c275bf7b4a6e5a1e96
irAbs174 <Arashsorosh174@gmail.com>
2024-07-25 23:23:12 +0000

    fix bugs +> comparison

--- app/core/crawler.py
+++ app/core/crawler.py
@@ -34,6 +34,9 @@ def perform_crawl():
         jobName = job.jobName
         jobArg = job.jobArg
         if jobName == 'crawl':
+            Product.objects.filter(product_parent=jobArg).delete()
+            SiteMap.objects.all().delete()
+
             sitemap_soup = crawler(f'{jobArg}/sitemap_index.xml')
             product_sitemap = get_products_sitemap(sitemap_soup)
             for i in product_sitemap:
@@ -50,16 +53,49 @@ def perform_crawl():
 
             for i in product_urls:
                 info = get_product_info(i)
-                Product.objects.all().filter(product_parent=jobArg).update(
+                Product.objects.all().filter(product_parent=jobArg, product_url=i).update(
                     product_name=info['name'],
                     product_price=info['price'],
                     product_stock=info['status'],
-                    product_url=jobArg,
                 )
                 print(f'save product detail {info}')
 
-        # Add your job logic here
-        time.sleep(3 * 60 * 60)  # Sleep for 3 hours
+            LogModel.objects.create(
+                logName = f"{jobArg} => crawl-camplated",
+                logType="notification",
+                scanedProducts = f'{len(product.objects.filter(product_parent=jobArg))}',
+            )
+
+            JobsModel.objects.all().first().delete()
+
+def comparison():
+    our_product_name = input('enter our_product_name: \n')
+    our_product_price = input('enter our_product_price: \n')
+    main_dic = Product.objects.all()
+    for i in main_dic:
+        if i.product_name.find(our_product_name):
+            print(f'product : {our_product_name} found !')
+            if int(our_product_price) < int(i.product_price):
+                print(f'PRODUCT DOWN !! => {our_product_name} < {i.product_name}')
+                LogModel.objects.create(
+                    logName="down",
+                    logType= f'{our_product_name}<{i.product_name}',
+                )
+                Product.objects.filter(product_name=i.product_name).update(
+                    product_status="down",
+                )
+            elif int(our_product_price) == int(i.price):
+                print(f'equals price => {our_product_price} = {i.price}')
+                Product.objects.filter(product_name=i.product_name).update(
+                    product_status="equals",
+                )
+            else:
+                print(f'{our_product_price} normal price')
+                Product.objects.filter(product_name=i.product_name).update(
+                    product_status="up",
+                )
+        else:
+            print(f'product not exist! => {our_product_name} <=')
 
 # Start the HTTP server
 def start_server():
@@ -69,61 +105,3 @@ def start_server():
         httpd.serve_forever()
 
 start_server()
-
-'''
-def handle_client(client_socket):
-    while True:
-        try:
-
-                client_socket.send(f"target=>{jobArg}".encode('utf-8'))
-                message = client_socket.recv(1024).decode('utf-8')
-
-                if 'sitemap_url=>' in message:
-
-                
-                if 'give_me_sitemap_to_crawl' in message:
-                    for i in SiteMap.objects.filter(target=jobArg):
-                        client_socket.send(f'sitemap_to_crawl=>{i.siteMapUrl}'.encode('utf-8'))
-                        time.sleep(5)
-
-                if 'product_url=>' in message:
-                    url = message.split('=>')[1]
-                    
-                    client_socket.send(f'get_product_info=>{url}'.encode('utf-8'))
-
-                if 'give_me_product_url' in message:
-
-                if 'product_info=>' in message:
-                    msg = message.split('=>')
-                    product_name = msg[1]
-                    product_price = msg[2]
-                    product_stock = msg[3]
-                    product_url = msg[4]
-                    Product.objects.filter(product_url=product_url).update(
-                        product_name=product_name,
-                        product_price=product_price,
-                        product_stock=product_stock
-                    )
-                else:
-                    print('listening ...')
-        except Exception as e:
-            print(f"Error: {e}")
-            break
-    client_socket.close()
-
-def start_client():
-    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    client.bind(('0.0.0.0', 8778))
-    client.listen(5)
-    print("SERVER started, waiting for connections...")
-    
-    while True:
-        client_socket, addr = client.accept()
-        print(f"Accepted connection from {addr}")
-        client_handler = threading.Thread(target=handle_client, args=(client_socket,))
-        client_handler.start()
-
-if __name__ == "__main__":
-    start_client()
-
-'''
