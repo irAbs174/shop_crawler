aab06c8f4ca1e5502453f983a1166ba4a11acc15
irAbs174 <Arashsorosh174@gmail.com>
2024-07-25 20:01:33 +0000

    fix socket bug => while

--- app/core/server.py
+++ app/core/server.py
@@ -4,7 +4,6 @@ import time
 import django
 import sys
 import os
-import time
 
 sys.path.append('/home/arashsorosh175/shop_crawler/app/core')
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings")
@@ -19,53 +18,49 @@ from logs.models import LogModel
 def handle_client(client_socket):
     while True:
         try:
-            jobName = JobsModel.objects.all().first().jobName
-            jobArg = JobsModel.objects.all().first().jobArg
+            job = JobsModel.objects.first()
+            jobName = job.jobName
+            jobArg = job.jobArg
             
             if jobName == 'crawl':
                 client_socket.send(f"target=>{jobArg}".encode('utf-8'))
                 message = client_socket.recv(1024).decode('utf-8')
 
-                while 'sitemap_url=>' in message:
-                    sitemap = message.decode('utf-8').split('=>')[1]
-                    SiteMap.objects.create(
-                        target=jobArg,
-                        siteMapUrl=sitemap,
-                    )
+                if 'sitemap_url=>' in message:
+                    sitemap = message.split('=>')[1]
+                    SiteMap.objects.create(target=jobArg, siteMapUrl=sitemap)
                     print(f'site map {sitemap} saved to db')
                 
-                while 'give_me_sitemap_to_crawl' in message:
-                    for i in SiteMap.objects.all().filter(target=jobArg):
+                if 'give_me_sitemap_to_crawl' in message:
+                    for i in SiteMap.objects.filter(target=jobArg):
                         client_socket.send(f'sitemap_to_crawl=>{i.siteMapUrl}'.encode('utf-8'))
                         time.sleep(5)
 
-                while 'product_url=>' in message:
-                    url = message.decode('utf-8').split('=>')[1]
-                    Product.objects.create(
-                        product_url=url,
-                        product_parent=jobArg,
-                    )
+                if 'product_url=>' in message:
+                    url = message.split('=>')[1]
+                    Product.objects.create(product_url=url, product_parent=jobArg)
                     client_socket.send(f'get_product_info=>{url}'.encode('utf-8'))
 
-                while 'give_me_product_url' in message:
-                    url = Product.objects.filter(product_parent=jobArg, product_price='').first().product_url
-                    client_socket.send(url.encode('utf-8'))
+                if 'give_me_product_url' in message:
+                    product = Product.objects.filter(product_parent=jobArg, product_price='').first()
+                    if product:
+                        client_socket.send(f"product_url=>{product.product_url}".encode('utf-8'))
 
-                while 'product_info=>' in message:
-                    msg = message.decode('utf-8').split('=>')
+                if 'product_info=>' in message:
+                    msg = message.split('=>')
                     product_name = msg[1]
                     product_price = msg[2]
                     product_stock = msg[3]
                     product_url = msg[4]
-                    Products.objects.all().filter(product_url=product_url).update(
-                        product_name = product_name,
-                        product_price = product_price,
-                        product_stock = product_stock,
+                    Product.objects.filter(product_url=product_url).update(
+                        product_name=product_name,
+                        product_price=product_price,
+                        product_stock=product_stock
                     )
-                    
                 else:
-                    print('lissing ...')
-        except:
+                    print('listening ...')
+        except Exception as e:
+            print(f"Error: {e}")
             break
     client_socket.close()
 
--- app/crawler/stream/client.py
+++ app/crawler/stream/client.py
@@ -5,36 +5,42 @@ from functions import *
 
 def send_message(server_socket):
     while True:
-        response = server_socket.recv(1024).decode('utf-8')
-        print(f"SERVER_MSG => {response}")
-        while 'target=>' in response:
-            target = response.split('=>')[1]
-            print(f'Start crawling to {target}')
-            sitemap_soup = crawler(f'{target}/sitemap_index.xml')
-            products_sitemap = get_products_sitemap(sitemap_soup)
-            for i in products_sitemap:
-                server_socket.send(f"sitemap_url=>{i}".encode('utf-8'))
-                time.sleep(1.5)
-
-            server_socket.send("give_me_sitemap_to_crawl".encode('utf-8'))
+        try:
+            response = server_socket.recv(1024).decode('utf-8')
+            print(f"SERVER_MSG => {response}")
             
-        while 'sitemap_to_crawl=>' in response:
-            sitemap = response.split('=>')[1]
-            product_list = get_products_list(sitemap)
-            for i in product_list:
-                server_socket.send(f"product_url=>{i}".encode('utf-8'))
-                time.sleep(1.5)
+            if 'target=>' in response:
+                target = response.split('=>')[1]
+                print(f'Start crawling to {target}')
+                sitemap_soup = crawler(f'{target}/sitemap_index.xml')
+                products_sitemap = get_products_sitemap(sitemap_soup)
+                for i in products_sitemap:
+                    server_socket.send(f"sitemap_url=>{i}".encode('utf-8'))
+                    time.sleep(1.5)
 
-        while 'get_product_info=>' in message:
-            product_url = message.split('=>')[1]
-            info = get_product_info(product_url)
-            name = info['name']
-            price = info['price']
-            stock = info['status']
-            server_socket.send(f"product_info=>{name}=>{price}=>{stock}=>{product_url}")
-        else:
-            rint('waiting ...')
-        time.sleep(5)
+                server_socket.send("give_me_sitemap_to_crawl".encode('utf-8'))
+
+            elif 'sitemap_to_crawl=>' in response:
+                sitemap = response.split('=>')[1]
+                product_list = get_products_list(sitemap)
+                for i in product_list:
+                    server_socket.send(f"product_url=>{i}".encode('utf-8'))
+                    time.sleep(1.5)
+
+            elif 'get_product_info=>' in response:
+                product_url = response.split('=>')[1]
+                info = get_product_info(product_url)
+                name = info['name']
+                price = info['price']
+                stock = info['status']
+                server_socket.send(f"product_info=>{name}=>{price}=>{stock}=>{product_url}".encode('utf-8'))
+
+            else:
+                print('waiting ...')
+            time.sleep(5)
+        except Exception as e:
+            print(f"Error: {e}")
+            break
 
 def start_server():
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
