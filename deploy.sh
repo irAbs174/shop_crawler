ec5be55ed295f06125d366a5cb66b77c3719dde8
irAbs174 <damerchiloa@gmail.com>
2024-10-07 12:49:55 +0330

    MIDIFY NEW CHANGES => REMOVE REQUIREMENTS.TXT (I HATE) THATAND CRAETE DEPLOY.SH FILE FOR INSTALL REQUIREMENTS OF PROJECTS AND DEPLOY AND RUN

---
+++ deploy.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#GRAND ACCRESS TO THIS FILE WITH EXECUTE:
+#chmod 755 deploy.sh
+
+#EXECUTE THIS FILE TO INSTALL PROJECT REQUIREMENTS
+clear
+echo "SHOP CRAWLER V0.0.1 BY 174"
+
+#sudo apt install python3 python3-venv
+#python3 -m venv env
+#source env/bin/activate
+#pip3 install --upgrade pip && pip install django django-cors-headers bs4 sockets requests aiohttp kavenegar fake-useragent aiogram openpyxl khayyam
+
+python3 app/core/crawler.py
+
+
